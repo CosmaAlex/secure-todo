91b5defd28a0ef33874dd53ed3f74eee4969fe8b
Cosma Alex Vergari <cosmaalexvergari@gmail.com>
2021-11-21 12:13:33 +0100

    Fix GET in client

--- todo_client/client.py
+++ todo_client/client.py
@@ -1,1 +1,28 @@
-print("Hello World")
+#!/usr/bin/python3
+
+from requests_pkcs12 import get
+import os
+
+debug = 1
+
+host_addr = 'https://localhost'
+host_port = 9999
+host_endpoint = '/todos'
+client_cert = '../' + os.environ.get('CLIENT_CERT')
+server_cert = '../' + os.environ.get('SERVER_CERT')
+filename = "todo.txt"
+
+if debug:
+    print("Loading client certificate from : " + client_cert)
+    print("Loading server certificate from : " + server_cert)
+r = get(host_addr + ":" + str(host_port) + host_endpoint, pkcs12_filename=client_cert, pkcs12_password="", verify=server_cert)
+
+if r.status_code == 200:
+    print("Got response 200 from server. Saving whatever response content to file")
+    try:
+        with open(filename, "wb") as bf:
+            bf.write(r.content)
+            bf.close()
+    except:
+        print("Exception while writing to file");
+
